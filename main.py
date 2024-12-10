66f80daea3da0ae729ce6dfb82e05feace5c3db4
Ashley King <ashleymking567@gmail.com>
2024-12-10 10:50:46 -0500

    12/10 work

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+import pymysql
+from dynaconf import Dynaconf
+
+app = Flask(__name__)
+
+conf = Dynaconf(
+    settings_file = ["settings.toml"]
+)
+
+def connect_db():
+    conn = pymysql.connect(
+        host = "10.100.34.80",
+        database = "aking_duck_books",
+        user = "aking",
+        password = conf.password,
+        autocommit = True,
+        cursorclass = pymysql.cursors.DictCursor
+    )
+
+    return conn
+
+@app.route("/")
+def index():
+    return render_template("homepage.html.jinja")
+
+
+
+@app.route("/browse")
+def product_browse():
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM `Product` ;")
+
+    results = cursor.fetchall()
+
+
+    return render_template("browse.html.jinja", products = results)
