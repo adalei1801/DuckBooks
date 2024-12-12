dc53b1a7377a5cfbdd2fafb64fd994633e974a08
Ashley King <ashleymking567@gmail.com>
2024-12-12 10:51:14 -0500

    12/12 work

--- main.py
+++ main.py
@@ -36,5 +36,23 @@ def product_browse():
 
     results = cursor.fetchall()
 
+    cursor.close()
+    conn.close()
 
     return render_template("browse.html.jinja", products = results)
+
+
+@app.route("/product/<product_id>")
+def product_page(product_id):
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM `Product` WHERE `id` = {product_id} ;")
+
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return render_template("product.html.jinja", product = result)
