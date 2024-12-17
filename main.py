33fbeca94914c333db04ba1a6667a80d4bbc8ca9
Ashley King <ashleymking567@gmail.com>
2024-12-17 10:51:07 -0500

    12/17 work

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 import pymysql
 from dynaconf import Dynaconf
 
@@ -7,6 +7,8 @@ app = Flask(__name__)
 conf = Dynaconf(
     settings_file = ["settings.toml"]
 )
+
+app.secret_key = conf.secret_key
 
 def connect_db():
     conn = pymysql.connect(
@@ -56,3 +58,51 @@ def product_page(product_id):
     conn.close()
 
     return render_template("product.html.jinja", product = result)
+
+@app.route("/signin")
+def signin():
+    return render_template("signin.html.jinja")
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if request.method == "POST":
+        name = request.form["name"]
+        username = request.form["username"]
+        password = request.form["password"]
+        con_pass = request.form["con_pass"]
+        email = request.form["email"]
+        address = request.form["address"]
+        card_num = request.form["card_num"]
+        phone = request.form["phone"]
+        birthday = request.form["bday"]
+        country = request.form["country"]
+
+        if password != con_pass:
+            flash("Password does not match")
+            return render_template("signup.html.jinja")
+        
+        if len(password) < 8:
+            flash("Password is too short")
+            return render_template("signup.html.jinja")
+        
+
+        conn = connect_db()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(f""" 
+            INSERT INTO `Customer`
+                (`name`, `username`, `password`, `card_number`, `address`, `email`, `birthday`, `country`, `phone_number`)
+            VALUES
+                ('{name}', '{username}', '{password}', '{card_num}', '{address}', '{email}', '{birthday}', '{country}', '{phone}' );
+            """)
+        except pymysql.err.IntegrityError:
+            flash("Sorry, that value is already in use")
+        else: 
+            return redirect("/signin")
+        finally:
+            cursor.close()
+            conn.close
+    
+
+    return render_template("signup.html.jinja")
